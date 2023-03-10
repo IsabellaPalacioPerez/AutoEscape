2f1688988b5585ef2d58c3136eb89c7a1722c9d3
Carlos Andrés Escobar López <carlosa.escobar@upb.edu.co>
2023-03-10 09:46:44 -0500

    Avance Clase 10/03/23

--- Game/Assets/Game/Assets/Scripts/ControlPickUp1.cs
+++ Game/Assets/Game/Assets/Scripts/ControlPickUp1.cs
@@ -5,29 +5,30 @@ using TMPro;
 
 public class ControlPickUp1 : MonoBehaviour
 {
-  
     [SerializeField]
-    private TMP_Text txt_contador_pickUp1;
-    private int contador_pickUp1;
+    private TMP_Text txt_contador_PickUp1;
+    private int contador_PickUp1;
 
+    public void Start()
+    {
+        
+    }
+    public int CantidadPickUp1Recolectados()
+    {
+        return contador_PickUp1;
+    }
     public void ActualizarPuntaje(int valor)
     {
-        contador_pickUp1 += valor;
+        contador_PickUp1 += valor;
         ActualizarValorUI();
-
-     
     }
-
+    public void ResetearPuntaje()
+    {
+        contador_PickUp1 = 0;
+        ActualizarValorUI();
+    }
     public void ActualizarValorUI()
     {
-        txt_contador_pickUp1.text = "" + contador_pickUp1;
-    }
-    public void ActualizarContador(int valor)
-    {
-        contador_pickUp1 += valor;
-    }
-    public int ContadorPickup()
-    {
-        return contador_pickUp1;
+        txt_contador_PickUp1.text = ""+ contador_PickUp1;
     }
 }
