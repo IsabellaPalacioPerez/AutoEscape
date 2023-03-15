4f9bf7ba2a55aae9e69c44fa986246f1206735ad
Isabella Palacio Pérez <isabella.palacio@upb.edu.co>
2023-03-15 08:42:35 -0500

    Tiempo

---
+++ Game/Assets/Game/Assets/Scripts/ControlTiempo.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ControlTiempo : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text txt_contador_Tiempo;
+    private int contador_Tiempo = 300;
+    private int i = 0;
+
+  
+    void Start()
+    {
+        Invoke("TerminarTemporizador", 300f);
+
+        InvokeRepeating("ActualizarTiempo", 0f,1f);
+    }
+
+    public void ActualizarTiempo()
+    {
+      
+        
+            contador_Tiempo -= 1;
+            ActualizarValorUI();
+        
+        
+    }
+    public void ActualizarValorUI()
+    {
+        txt_contador_Tiempo.text = "" + contador_Tiempo;
+    }
+    void TerminarTemporizador()
+    {
+        Debug.Log("Se terminó el tiempo");
+
+    }
+}
