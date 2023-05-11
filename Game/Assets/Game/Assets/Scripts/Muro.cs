94b7c2bb15158de30dda176dbddbcaca5c71c961
Isabella Palacio Pérez <isabella.palacio@upb.edu.co>
2023-05-11 11:22:47 -0500

    .

--- Game/Assets/Game/Assets/Scripts/Muro.cs
+++ Game/Assets/Game/Assets/Scripts/Muro.cs
@@ -7,20 +7,19 @@ public class Muro : MonoBehaviour
 
     [SerializeField]
     private GameObject muro;
-    private ControlPickUp1 controlPickUp1;
+    private ControlPickUp1 ctrlPickUp1;
     
     public void Start()
     {
-        controlPickUp1 = this.transform.parent.GetComponent<ControlPickUp1>();
+        ctrlPickUp1 = this.transform.parent.GetComponent<ControlPickUp1>();
         
     }
     public void Update()
     {
-        if (controlPickUp1.CantidadPickUp1Recolectados() == 15)
+        if (ctrlPickUp1.CantidadPickUp1Recolectados() == 15)
         {
             CambiarEstado(false);
         }
-
     }
     public void CambiarEstado(bool estado)
     {
